0a364ac1a394459ffa011757de4e8cd290ceb789
Rashmi Pathinayaka <140165202+rashmipathinayaka@users.noreply.github.com>
2025-01-30 11:07:43 +0530

    edited topmeels

---
+++ src/component/Home/topMeel.js
@@ -0,0 +1,25 @@
+export const topMeels=[
+{
+    image:"https://cdn.pixabay.com/photos/pizza-italian-homemade-cheese-3007395/",
+    title:"pizza"
+},
+{
+    image:"https://pixabay.com/photos/cake-berries-dessert-sweet-8233676/",
+    title:"cake"
+},
+{
+    image:"https://cdn.pixabay.com/photo/2022/07/15/18/16/beef-burger-7323692_1280.jpg",
+    title:"burger"
+},
+{
+    image:"https://media.istockphoto.com/id/1460788317/photo/south-indian-vegetarian-breakfast.jpg?s=612x612&w=0&k=20&c=uWbKr62yUQ4xf7_rzfEjEq1ELk-vYyO1obLTvd6E-3s=",
+    title:"dosa"
+},
+
+{
+    image:"https://media.istockphoto.com/id/157431311/photo/turkey-sandwich.jpg?s=612x612&w=0&k=20&c=uB6byErFAnWxFkkAqMiGNRJGE8r3nqsSDdqrfBE8HOA=",
+    title:"sandwich"
+}
+
+    
+];
